b58ef332e7c5705bd5aa7c7cb3c8381165a85018
wololowarrior <harshilgupta1808@gmail.com>
2016-11-02 14:24:09 +0530

    Latest Work till sessionals

---
+++ app/src/main/java/honf/harshil/com/honf/TimePickerFragment.java
@@ -0,0 +1,43 @@
+package honf.harshil.com.honf;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+/**
+ * Created by harshil on 28.10.16.
+ */
+
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c=Calendar.getInstance();
+        int hour=c.get(Calendar.HOUR);
+        int min=c.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(),this,hour,min, DateFormat.is24HourFormat(getActivity()));
+    }
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        EditText et = (EditText) getActivity().findViewById(R.id.openingTime);
+        EditText et2 = (EditText) getActivity().findViewById(R.id.closingTime);
+        if (et.isEnabled()) {
+            et.setText(String.valueOf(hourOfDay) +
+                    ":" + String.valueOf(minute));
+        }
+        else if(et2.isEnabled()){
+            et.setText(String.valueOf(hourOfDay) +
+                    ":" + String.valueOf(minute));
+        }
+    }
+
+}
+
